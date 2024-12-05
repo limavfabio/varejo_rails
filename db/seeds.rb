095ec45147e7b6c8c9c21adc9831655ad5ca44b2
Fábio Vinícius Lima <limavfabio@gmail.com>
2024-12-05 08:54:42 -0300

    Update seeds for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,109 +1,103 @@
 # db/seeds.rb
 require "faker"
 
-# Create Companies
-10.times do
-  Company.create!(
-    name: Faker::Company.name
+if Rails.env.development?
+  # Create Companies
+  10.times do
+    Company.create!(
+      name: Faker::Company.name
+    )
+  end
+
+  # Create an Admin User
+  User.create!(
+    name: "Admin",
+    email: "admin@admin.com",
+    password: "password",
+    verified: true,
+    role: 2, # Assuming 2 is the admin role
+    company: Company.first
   )
-end
 
-# Create an Admin User
-User.create!(
-  name: "Admin",
-  email: "admin@admin.com",
-  password: "password",
-  verified: true,
-  role: 2, # Assuming 2 is the admin role
-  company: Company.first
-)
-
-# Create Users
-companies = Company.all
-5.times do
-  companies.each do |company|
-    User.create!(
-      name: Faker::Name.name,
-      email: Faker::Internet.unique.email,
-      password: "password", # Use a secure password in production
-      verified: Faker::Boolean.boolean,
-      role: rand(0..2), # Assuming roles are defined as integers
-      company: company
-    )
-  end
-end
-
-# Create Customers
-companies.each do |company|
+  # Create Users
+  companies = Company.all
   5.times do
-    Customer.create!(
-      name: Faker::Name.name,
-      address: Faker::Address.full_address,
-      company: company
-    )
-  end
-end
-
-# Create Payment Methods
-companies.each do |company|
-  payment_methods = [ "Credit Card", "Debit Card", "Cash", "PIX" ]
-  payment_methods.each do |payment_method|
-    PaymentMethod.create!(name: payment_method, company: company)
-  end
-end
-
-# Create Products
-companies.each do |company|
-  5.times do
-    Product.create!(
-      name: Faker::Commerce.product_name,
-      description: Faker::Commerce.department(max: 1),
-      retail_price: Faker::Commerce.price(range: 50.0..100.0),
-      company: company
-    )
-  end
-end
-
-# t.string "description"
-# t.integer "operation"
-# t.integer "operation_type"
-# t.datetime "created_at", null: false
-# t.datetime "updated_at", null: false
-# Create Fiscal Scenarios
-companies.each do |company|
-  FiscalScenario.create!(description: "Venda", operation: 1, operation_type: 1, company: company)
-end
-
-# Create Fiscal Documents
-companies.each do |company|
-  5.times do
-    customer = Customer.where(company: company).sample
-    fiscal_document = FiscalDocument.create!(
-      company: company,
-      fiscal_scenario: FiscalScenario.where(company: company).sample,
-      customer: customer,
-      description: Faker::Lorem.sentence,
-      total_value: 0 # Will be updated after creating document items
-    )
-
-    # Create Document Items
-    2.times do
-      product = Product.where(company: company).sample
-      DocumentItem.create!(
-        fiscal_document: fiscal_document,
-        product: product,
-        quantity: rand(1..5)
+    companies.each do |company|
+      User.create!(
+        name: Faker::Name.name,
+        email: Faker::Internet.unique.email,
+        password: "password", # Use a secure password in production
+        verified: Faker::Boolean.boolean,
+        role: rand(0..2), # Assuming roles are defined as integers
+        company: company
       )
     end
+  end
 
-    # Calculate and update total value
-    total_value = fiscal_document.document_items.sum do |item|
-      item.product.retail_price * item.quantity
+  # Create Customers
+  companies.each do |company|
+    5.times do
+      Customer.create!(
+        name: Faker::Name.name,
+        address: Faker::Address.full_address,
+        company: company
+      )
     end
-    fiscal_document.update(total_value: total_value)
+  end
 
-    # Create Document Payments
-    1.times do
+  # Create Payment Methods
+  companies.each do |company|
+    payment_methods = ["Credit Card", "Debit Card", "Cash", "PIX"]
+    payment_methods.each do |payment_method|
+      PaymentMethod.create!(name: payment_method, company: company)
+    end
+  end
+
+  # Create Products
+  companies.each do |company|
+    5.times do
+      Product.create!(
+        name: Faker::Commerce.product_name,
+        description: Faker::Commerce.department(max: 1),
+        retail_price: Faker::Commerce.price(range: 50.0..100.0),
+        company: company
+      )
+    end
+  end
+
+  # t.string "description"
+  # t.integer "operation"
+  # t.integer "operation_type"
+  # t.datetime "created_at", null: false
+  # t.datetime "updated_at", null: false
+  # Create Fiscal Scenarios
+  companies.each do |company|
+    FiscalScenario.create!(description: "Venda", operation: 1, operation_type: 1, company: company)
+  end
+
+  # Create Fiscal Documents
+  companies.each do |company|
+    5.times do
+      customer = Customer.where(company: company).sample
+      fiscal_document = FiscalDocument.create!(
+        company: company,
+        fiscal_scenario: FiscalScenario.where(company: company).sample,
+        customer: customer,
+        description: Faker::Lorem.sentence,
+        total_value: 0 # Will be updated after creating document items
+      )
+
+      # Create Document Items
+      2.times do
+        product = Product.where(company: company).sample
+        DocumentItem.create!(
+          fiscal_document: fiscal_document,
+          product: product,
+          quantity: rand(1..5)
+        )
+      end
+
+      # Create Document Payments
       payment_method = PaymentMethod.where(company: company).sample
       DocumentPayment.create!(
         fiscal_document: fiscal_document,
@@ -112,16 +106,43 @@ companies.each do |company|
       )
     end
   end
-end
 
-# Create Sessions
-# users = User.all
-# users.each do |user|
-#   3.times do
-#     Session.create!(
-#       user: user,
-#       user_agent: Faker::Internet.user_agent,
-#       ip_address: Faker::Internet.ip_v4_address
-#     )
-#   end
-# end
+  # Create Sessions
+  # users = User.all
+  # users.each do |user|
+  #   3.times do
+  #     Session.create!(
+  #       user: user,
+  #       user_agent: Faker::Internet.user_agent,
+  #       ip_address: Faker::Internet.ip_v4_address
+  #     )
+  #   end
+  # end
+
+elsif Rails.env.production?
+  company = Company.create!(name: "NBD")
+
+  FiscalScenario.create!(description: "Venda", operation: 1, operation_type: 1, company: company)
+
+  payment_methods = ["Crédito", "Débito", "Dinheiro", "PIX"]
+  payment_methods.each do |payment_method|
+    PaymentMethod.create!(name: payment_method, company: company)
+  end
+
+  User.create!(
+    name: "Admin",
+    email: "admin@admin.com",
+    password: "password", # Use a secure password in production
+    verified: true,
+    role: 2, # Assuming roles are defined as integers
+    company: company
+  )
+
+  10.times do
+    Customer.create!(
+      name: Faker::Name.name,
+      address: Faker::Address.full_address,
+      company: company
+    )
+  end
+end
