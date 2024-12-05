077431cb0cd79f3132576c8244657b41320d543d
Fábio Vinícius Lima <limavfabio@gmail.com>
2024-12-05 11:01:59 -0300

    scope customers to company

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,8 @@ class Customer < ApplicationRecord
   has_many :fiscal_documents
 
   def self.ransackable_attributes(auth_object = nil)
-    [ "address", "company_id", "created_at", "id", "name", "updated_at" ]
+    ["address", "company_id", "created_at", "id", "name", "updated_at"]
   end
+
+  default_scope { where(company_id: Current.user.company_id) }
 end
